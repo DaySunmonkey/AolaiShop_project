05e6dea068e3b3a45cc30584ae5b87371ceac0f7
DaySunmonky <798425764@qq.com>
2021-04-10 14:05:42 +0800

    初始化项目

---
+++ scripts/test_login.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+from AolaiShop_project.base.base_driver import init_driver
+
+
+class TestLogin():
+
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        sleep(2)
+        self.driver.quit()
+
+    def test_login(self):
+        print('hello')
